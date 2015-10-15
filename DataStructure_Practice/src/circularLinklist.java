676b3eb5bc9cc82d324a61270db6d2d3b45f2905
Harshal Jagtap <hdj3@njit.edu>
2015-10-14 21:16:20 -0700

    v1.1

--- DataStructure_Practice/src/circularLinklist.java
+++ DataStructure_Practice/src/circularLinklist.java
@@ -1,6 +1,29 @@
 import java.util.*;
 public class circularLinklist {
       public boolean hascycle(ListNode head){
+    	  ListNode fast = head;
+    	  ListNode slow = head;
+    	  
+    	  if(head==null){
+    		  return false;
+    	  }
+    	  
+    	  if(head.next==null){
+    		  return false;
+    		   }
+    	  while(fast!=null&&fast.next!=null){
+    		  slow=slow.next;
+    		  fast=fast.next.next;
+    		  
+    		  if(slow==fast){
+    			  return true;
+    		  }
+    		  
+    		 // return false;
+    	  }
+		return false;
+    	  
+      }
     	  
 	
 }
